ff8bd6dfb0790ba6dbc5f8c3916dcd7b2c641fb4
Kongpob <kongpop.rak@hotmail.co.th>
2020-02-24 14:53:46 +0700

    preparing path and file for homepage

--- s6101012610029/urls.py
+++ s6101012610029/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from calculator import views as calculator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', calculator.home),
 ]
