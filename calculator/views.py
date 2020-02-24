de9df2bbb080e6ea904a02369e229e21c4ae54fe
Kongpob <kongpop.rak@hotmail.co.th>
2020-02-24 15:09:26 +0700

    adding form and view function for X and Y inputs

--- calculator/views.py
+++ calculator/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    result = 0
+    if request.method == 'POST':
+        X = int(request.POST.get('X'))
+        Y = int(request.POST.get('Y'))
+        result = X + Y
+    return render(request, 'home.html', {'result': result})
