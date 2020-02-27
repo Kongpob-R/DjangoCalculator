52f00e8079b18e89242d3625cf38f8c919a9c5c6
Kongpob <kongpop.rak@hotmail.co.th>
2020-02-27 15:03:57 +0700

    add calculator feature to views function

--- calculatorGET/views.py
+++ calculatorGET/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request, equation = 0):
-    return render(request, "homeGET.html")
+def home(request, equation = "notthing!"):
+    result = "unknow"
+    if len(equation.split('+')) > 1:
+        eqnList = equation.split('+')
+        x = float(eqnList[0]) 
+        y = float(eqnList[1])
+        result = x + y
+    elif len(equation.split('-')) > 1:
+        eqnList = equation.split('-')
+        x = float(eqnList[0]) 
+        y = float(eqnList[1])
+        result = x - y
+    elif len(equation.split('*')) > 1:
+        eqnList = equation.split('*')
+        x = float(eqnList[0]) 
+        y = float(eqnList[1])
+        result = x * y
+    elif len(equation.split('%')) > 1:
+        eqnList = equation.split('%')
+        x = float(eqnList[0]) 
+        y = float(eqnList[1])
+        result = x / y
+    return render(request, "homeGET.html", {'equation':equation, 'result':result})
